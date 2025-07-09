8977601be5e8b3f722c3665cd90758d565ca5999
0561joni <31111927+0561joni@users.noreply.github.com>
2025-07-09 16:40:26 +0200

    Add autocomplete attributes to AuthForm inputs

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -75,6 +75,7 @@ export function AuthForm() {
               <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
               <input
                 type="email"
+                autoComplete="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
@@ -91,6 +92,7 @@ export function AuthForm() {
               <Lock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
               <input
                 type="password"
+                autoComplete="current-password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
@@ -136,5 +138,4 @@ export function AuthForm() {
         </div>
       </div>
     </div>
-  );
-}
+  );}
